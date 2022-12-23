462d13d664dd98cb1604da8cac79617c543e7b66
Breno Carvalho <brenocarvalho709@gmail.com>
2022-12-22 23:24:44 -0300

    updateTable

--- src/locator/locator.entity.ts
+++ src/locator/locator.entity.ts
@@ -5,64 +5,64 @@ export class Locator {
   @PrimaryColumn('int')
   locatorCode: number;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   provisionService: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   fullName: string;
 
-  @Column('date')
+  @Column({ type: 'date', nullable: true })
   birthDate: Date;
 
-  @Column('bigint')
+  @Column({ type: 'bigint', nullable: true })
   rg: number;
 
-  @Column('bigint')
+  @Column({ type: 'bigint', nullable: true })
   cpf: number;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   nationality: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   maritalStatus: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   profession: string;
 
   @Column({ length: 100, nullable: true })
   email: string;
 
-  @Column('bigint')
+  @Column({ type: 'bigint', nullable: true })
   contact1: number;
 
   @Column({ type: 'bigint', nullable: true })
   contact2: number;
 
-  @Column('bigint')
+  @Column({ type: 'bigint', nullable: true })
   cep: number;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   address: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   city: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   district: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   bank: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   accountType: string;
 
-  @Column('int')
+  @Column({ type: 'int', nullable: true })
   agency: number;
 
-  @Column('int')
+  @Column({ type: 'int', nullable: true })
   accountNumber: number;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   paymentRemittance: string;
 
   @CreateDateColumn()
