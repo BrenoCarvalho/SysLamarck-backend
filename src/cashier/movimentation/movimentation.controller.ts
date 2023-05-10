be8f20e727659260a1fe2017fe9200047a18346a
Breno Carvalho <brenocarvalho709@gmail.com>
2023-05-10 01:23:45 -0300

    update

--- src/cashier/movimentation/movimentation.controller.ts
+++ src/cashier/movimentation/movimentation.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { MovimentationService } from './movimentation.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { MovimentationCreateDto } from './dto/movimentation.create.dto';
@@ -20,38 +28,9 @@ export class MovimentationController {
     return this.movimentationService.findAll();
   }
 
-  // @UseGuards(JwtAuthGuard)
-  // @Get('findById/:id')
-  // async findOne(@Param() params): Promise<Property> {
-  //   return this.propertyService.findOne(params.id);
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  // @Get('findByPropertyCode/:propertyCode')
-  // async findByPropertyCode(@Param() params): Promise<Property> {
-  //   return this.propertyService.findOneBy({
-  //     propertyCode: params.propertyCode,
-  //   });
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  // @Put(':id')
-  // async update(
-  //   @Param() params,
-  //   @Body() data: PropertyCreateDto,
-  // ): Promise<string> {
-  //   return this.propertyService.update(params.id, data);
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  // @Post()
-  // async create(@Body() data: PropertyCreateDto): Promise<string> {
-  //   return await this.propertyService.create(data);
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  // @Delete(':id')
-  // async delete(@Param() params): Promise<number> {
-  //   return await this.propertyService.delete(params.id);
-  // }
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async delete(@Param() params): Promise<number> {
+    return await this.movimentationService.delete(params.id);
+  }
 }
--- src/cashier/movimentation/movimentation.service.ts
+++ src/cashier/movimentation/movimentation.service.ts
@@ -10,115 +10,9 @@ export class MovimentationService {
     private movimentationRepository: Repository<Movimentation>,
   ) {}
 
-  // async findAll(conditions?: any): Promise<Property[]> {
-  //   return this.propertyRepository.find(conditions);
-  // }
-
-  // async findBy(by: object): Promise<Property[]> {
-  //   return await this.propertyRepository.findBy(by);
-  // }
-
-  // async findOneBy(by: object): Promise<Property> {
-  //   return JSON.stringify(by) != '{}'
-  //     ? await this.propertyRepository.findOneBy(by)
-  //     : null;
-  // }
-
-  // async delete(id: number): Promise<number> {
-  //   const propertyCode = await (
-  //     await this.propertyRepository.findOneBy({ id })
-  //   ).propertyCode;
-
-  //   const tenants = await this.tenantService.findBy({
-  //     propertyCode: propertyCode,
-  //   });
-
-  //   tenants.map(async (value) => {
-  //     await this.tenantService.delete(value?.tenantCode);
-  //   });
-
-  //   return (await this.propertyRepository.delete(id)).affected;
-  // }
-
-  // async findOne(id: number): Promise<Property> {
-  //   return this.propertyRepository.findOneBy({ id: id });
-  // }
-
-  // async update(id: number, data: PropertyCreateDto): Promise<string> {
-  //   const property = await this.propertyRepository.findOneBy({ id: id });
-
-  //   if (!property) {
-  //     throw new NotFoundException(`Property ${id} not found`);
-  //   }
-
-  //   data.locatorName = await this.getLocatorName(data.locatorCode);
-
-  //   data.propertyCode = `${String(data.locatorCode).padStart(3, '0')}${String(
-  //     property.property,
-  //   ).padStart(3, '0')}`;
-
-  //   return this.propertyRepository
-  //     .update({ id: id }, data)
-  //     .then(() => {
-  //       const msg = `Property ${id} updated as successfuly`;
-  //       console.log(msg);
-
-  //       return msg;
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.driverError.sqlMessage);
-
-  //       throw new HttpException(
-  //         error.driverError.sqlMessage,
-  //         HttpStatus.INTERNAL_SERVER_ERROR,
-  //       );
-  //     });
-  // }
-
-  // async generatePropertyId(): Promise<number> {
-  //   const response = await this.propertyRepository.find({
-  //     select: { id: true },
-  //   });
-
-  //   const propertyCode = [];
-
-  //   response.map((value) => {
-  //     propertyCode.push(value.id);
-  //   });
-
-  //   let code = null;
-  //   let stop = false;
-
-  //   if (Math.min(...propertyCode) > 1) {
-  //     code = 1;
-  //   } else {
-  //     propertyCode.map((value, index) => {
-  //       if (!stop && propertyCode[index + 1] != value + 1) {
-  //         code = value + 1;
-  //         stop = true;
-  //       }
-  //     });
-  //   }
-
-  //   return code;
-  // }
-
-  // async generateProperty(locatorCode: number): Promise<number> {
-  //   const response = await this.propertyRepository.findBy({ locatorCode });
-
-  //   const properties = [];
-
-  //   response.map((value) => {
-  //     properties.push(value.property);
-  //   });
-
-  //   return properties.length ? Math.max(...properties) + 1 : 1;
-  // }
-
-  // async getLocatorName(locatorCode: number): Promise<string> {
-  //   const locator: Locator = await this.locatorService.findOne(locatorCode);
-  //   return locator?.fullName;
-  // }
+  async delete(id: number): Promise<number> {
+    return (await this.movimentationRepository.delete(id)).affected;
+  }
 
   async findAll(conditions?: any): Promise<Movimentation[]> {
     return this.movimentationRepository.find(conditions);
@@ -129,8 +23,8 @@ export class MovimentationService {
 
     movimentation.description = data?.description;
     movimentation.date = data?.date;
-    movimentation.credit = Number(data?.credit) || null;
-    movimentation.debit = Number(data?.debit) || null;
+    movimentation.credit = data?.credit;
+    movimentation.debit = data?.debit;
 
     return this.movimentationRepository
       .save(movimentation)
