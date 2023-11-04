6d35d76dfef8f6265502b72382136a2bde3c3750
Breno Carvalho <brenocarvalho709@gmail.com>
2023-11-04 19:04:02 -0300

    update

--- src/templates/rentReceipt.ts
+++ src/templates/rentReceipt.ts
@@ -6,6 +6,7 @@ const htmlTenantVersion = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="http://necolas.github.io/normalize.css/3.0.1/normalize.css">
     <style media="print">
         @page {
             margin: 0;
@@ -91,223 +92,219 @@ const htmlTenantVersion = `<!DOCTYPE html>
 
 const htmlLocatorVersion = `<!DOCTYPE html>
 <html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="http://necolas.github.io/normalize.css/3.0.1/normalize.css">
+    <style media="print">
+      @page {
+            margin: 0;
+            size: auto;
+        }
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <style media="print">
-    @page {
-      margin: 0;
-      size: auto;
-    }
+      body,
+      html {
+        font: 11px normal, arial;
+        margin: -2;
+        size: auto;
+      }
 
-    body,
-    html {
-      font: 11px normal, arial;
-      margin: 0;
-      padding: 0;
-    }
+      div,p {
+        margin: 1px 0;
+      }
+      img {
+          width: 100px;
+        }
+      p {
+          margin-left: 5px;
+      }
+      .cabeca {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      }
 
-    div,
-    p {
-      margin: 1px 0;
-    }
+      .container {
+        padding: 0;
+        margin: 0;
+        height: 50vh;
 
-    img {
-      width: 100px;
-    }
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      }
 
-    p {
-      margin-left: 5px;
-    }
+      .section{
+        flex: 1;
+        max-width: 49%;
+      }
 
-    .cabeca {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
+      .containerItens {
+        display: flex;
+        justify-content: space-between;
+      }
 
-    .container {
-      padding: 0;
-      margin: 0;
-      height: 50vh;
+      .linha {
+        width: 100%;
+        margin: 5px 0px;
+        border-bottom: 1px solid #000000;
+        border-top: 1px solid #000000;
+      }
 
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-    }
+      .linhaBaixo {
+        width: 100%;
+        border-bottom: 1px solid #000000;
+      }
 
-    .section {
-      flex: 1;
-      max-width: 49%;
-    }
-
-    .containerItens {
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .linha {
-      width: 100%;
-      margin: 5px 0px;
-      border-bottom: 1px solid #000000;
-      border-top: 1px solid #000000;
-    }
-
-    .linhaBaixo {
-      width: 100%;
-      border-bottom: 1px solid #000000;
-    }
-
-    .caixa {
-      flex: 1;
-      margin: 0 5px;
-      border: 1px solid #000000;
-    }
-  </style>
-</head>
-
-<body>
-  <div class="container">
-    <section class="section">
-      <header class="cabeca">
-        <img src="https://i.imgur.com/Roi8Q9V.png" />
-        <p>
-          Rua Dr. Carlos Autran N 15 - Centro, LORENA/SP. CEP. 12600-160 TEL
-          .: (12) 3152-1099 / 3152-1541 CRECI- 30558-J
-        </p>
-      </header>
-      <div class="linha">
-        <p style="text-align: center"><b>EXTRATO MENSAL</b></p>
-      </div>
-      <p><b>Locador:</b> {{locator.fullName}}</p>
-      <p><b>Imóvel:</b> {{property.address}}</p>
-      <p><b>Locatário:</b> {{tenant.fullName}}</p>
-      <div class="linhaBaixo"></div>
-      <p><b>Mês de Referência:</b> {{installment.referenceMonth}} / <b>Data de vencimento:</b> {{installment.dueDate}}
-      </p>
-      <p><b>Data Pagamento:</b> {{installment.paymentDate}} / <b>N° PARCELA:</b> {{installment.currentInstallment}} /
-        <b>CÓD:</b>{{property.propertyCode}}</p>
-      <div class="linha">
-        <p style="text-align: center"><b>IDENTIFICAÇÃO DOS ITENS</b></p>
-      </div>
-      <div class="containerItens">
-        <div>
-          <p><b><u>CRÉDITO</b></u></p>
-          <p><b>Aluguel:</b> {{creditTransaction.rent}}</p>
-          <p><b>IPTU:</b> {{creditTransaction.iptu}}</p>
-          <p><b>Água:</b> {{creditTransaction.water}}</p>
-          <p><b>Luz:</b> {{creditTransaction.eletricity}}</p>
-          <p><b>Condomínio:</b> {{creditTransaction.condominium}}</p>
-          <p><b>Imposto de renda:</b> {{creditTransaction.incomeTax}}</p>
-          <p><b>Diversos*:</b> {{creditTransaction.sundry}}</p>
-          <p><b>Multa recisória:</b> {{creditTransaction.breachOfContractFine}}</p>
+      .caixa {
+        flex: 1;
+        margin: 0 5px;
+        border: 1px solid #000000;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <section class="section">
+        <header class="cabeca">
+            <img src="https://i.imgur.com/Roi8Q9V.png" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <p style="font-size: 10px;">
+              Rua Dr. Carlos Autran N 15 - Centro, Lorena/SP. CEP. 12600-160 TEL
+              .: (12) 3152-1099 / 3152-1541 CRECI- 30558-J
+            </p>
+        </header>
+        <div class="linha">
+          <p style="text-align: center"><b>EXTRATO MENSAL</b></p>
         </div>
-        <div>
-          <p><b><u>DÉBITO</b></u></p>
-          <p><b>IPTU:</b> {{debitTransaction.iptu}}</p>
-          <p><b>Água:</b> {{debitTransaction.water}}</p>
-          <p><b>Luz:</b> {{debitTransaction.eletricity}}</p>
-          <p><b>Condomínio:</b> {{debitTransaction.condominium}}</p>
-          <p><b>Imposto de renda:</b> {{debitTransaction.incomeTax}}</p>
-          <p><b>Diversos*:</b> {{debitTransaction.sundry}}</p>
-          <p><b>Taxa ADM:</b> {{debitTransaction.administrationFee}}</p>
-          <p><b>Taxa Locação:</b> {{debitTransaction.rentalFee}}</p>
+        <p><b>Locador:</b> {{locator.fullName}}</p>
+        <p><b>Imóvel:</b> {{property.adress}}</p>
+        <p><b>Locatário:</b> {{tenant.fullName}}</p>
+        <div class="linhaBaixo"></div>
+        <p><b>Mês de Referência:</b> {{installment.referenceMonth}} / <b>Data de vencimento:</b> {{installment.dueDate}}</p>
+        <p><b>Data Pagamento:</b> {{installment.paymentDate}} / <b>N° PARCELA:</b> {{installment.currentInstallment}} / <b>CÓD:</b>{{property.propertyCode}}</p>
+        <div class="linha">
+          <p style="text-align: center"><b>IDENTIFICAÇÃO DOS ITENS</b></p>
         </div>
-      </div>
-      <br />
-      <div class="containerItens">
+        <div class="containerItens">
+          <div>
+            <p><b><u>CRÉDITO</b></u></p>
+            <p><b>Aluguel:</b> {{creditTransaction.rent}}</p>
+            <p><b>IPTU:</b> {{creditTransaction.iptu}}</p>
+            <p><b>Água:</b> {{creditTransaction.water}}</p>
+            <p><b>Luz:</b> {{creditTransaction.eletricity}}</p>
+            <p><b>Condomínio:</b> {{creditTransaction.condominium}}</p>
+            <p><b>Imposto de renda:</b> {{creditTransaction.incomeTax}}</p>
+            <p><b>Diversos*:</b> {{creditTransaction.sundry}}</p>
+            <p><b>Multa recisória:</b> {{creditTransaction.breachOfContractFine}}</p>
+            <p><b>TOTAL:</b> {{creditTransaction.amount}}</p>
+          </div>
+          <div>
+            <p><b><u>DÉBITO</b></u></p>
+            <p><b>IPTU:</b> {{debitTransaction.iptu}}</p>
+            <p><b>Água:</b> {{debitTransaction.water}}</p>
+            <p><b>Luz:</b> {{debitTransaction.eletricity}}</p>
+            <p><b>Condomínio:</b> {{debitTransaction.condominium}}</p>
+            <p><b>Imposto de renda:</b> {{debitTransaction.incomeTax}}</p>
+            <p><b>Diversos*:</b> {{debitTransaction.sundry}}</p>
+            <p><b>Taxa ADM:</b> {{debitTransaction.administrationFee}}</p>
+            <p><b>Taxa Locação:</b> {{debitTransaction.rentalFee}}</p>
+            <p><b>TOTAL:</b> {{debitTransaction.amount}}</p>
+          </div>
+        </div>
+        <div class="containerItens">
+          <div class="caixa">
+            <p><b>*Descrição do diversos em crédito*</b></p>
+            <p>{{creditTransaction.sundryDescription}}</p>
+          </div>
+          <div class="caixa">
+            <p><b>*Descrição do diversos em dédito*</b></p>
+            <p>{{debitTransaction.sundryDescription}}</p>
+          </div>
+        </div>
+        <div class="linha">
+          <p style="text-align: center"><b>SALDO:</b> {{creditTransaction.amount}} - {{debitTransaction.amount}}</p>
+        </div>
+        <br />
         <div class="caixa">
-          <p>{{creditTransaction.sundryDescription}}</p>
+          <p style="text-align: center">
+            <b>Banco:</b> {{locator.bank}} / <b>Tipo da conta:</b> {{locator.accountType}}
+          </p>
+          <p style="text-align: center">  
+            <b>Agência:</b> {{locator.agency}} / <b>Conta:</b> {{locator.accountNumber}}
+          </p>
         </div>
+      </section>
+      <section class="section">
+        <header class="cabeca">
+            <img src="https://i.imgur.com/Roi8Q9V.png" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <p style="font-size: 10px;">
+              Rua Dr. Carlos Autran N 15 - Centro, Lorena/SP. CEP. 12600-160 TEL
+              .: (12) 3152-1099 / 3152-1541 CRECI- 30558-J
+            </p>
+        </header>
+        <div class="linha">
+          <p style="text-align: center"><b>EXTRATO MENSAL</b></p>
+        </div>
+        <p><b>Locador:</b> {{locator.fullName}}</p>
+        <p><b>Imóvel:</b> {{property.adress}}</p>
+        <p><b>Locatário:</b> {{tenant.fullName}}</p>
+        <div class="linhaBaixo"></div>
+        <p><b>Mês de Referência:</b> {{installment.referenceMonth}} / <b>Data de vencimento:</b> {{installment.dueDate}}</p>
+        <p><b>Data Pagamento:</b> {{installment.paymentDate}} / <b>N° PARCELA:</b> {{installment.currentInstallment}} / <b>CÓD:</b>{{property.propertyCode}}</p>
+        <div class="linha">
+          <p style="text-align: center"><b>IDENTIFICAÇÃO DOS ITENS</b></p>
+        </div>
+        <div class="containerItens">
+          <div>
+            <p><b><u>CRÉDITO</b></u></p>
+            <p><b>Aluguel:</b> {{creditTransaction.rent}}</p>
+            <p><b>IPTU:</b> {{creditTransaction.iptu}}</p>
+            <p><b>Água:</b> {{creditTransaction.water}}</p>
+            <p><b>Luz:</b> {{creditTransaction.eletricity}}</p>
+            <p><b>Condomínio:</b> {{creditTransaction.condominium}}</p>
+            <p><b>Imposto de renda:</b> {{creditTransaction.incomeTax}}</p>
+            <p><b>Diversos*:</b> {{creditTransaction.sundry}}</p>
+            <p><b>Multa recisória:</b> {{creditTransaction.breachOfContractFine}}</p>
+            <p><b>TOTAL:</b> {{creditTransaction.amount}}</p>
+          </div>
+          <div>
+            <p><b><u>DÉBITO</b></u></p>
+            <p><b>IPTU:</b> {{debitTransaction.iptu}}</p>
+            <p><b>Água:</b> {{debitTransaction.water}}</p>
+            <p><b>Luz:</b> {{debitTransaction.eletricity}}</p>
+            <p><b>Condomínio:</b> {{debitTransaction.condominium}}</p>
+            <p><b>Imposto de renda:</b> {{debitTransaction.incomeTax}}</p>
+            <p><b>Diversos*:</b> {{debitTransaction.sundry}}</p>
+            <p><b>Taxa ADM:</b> {{debitTransaction.administrationFee}}</p>
+            <p><b>Taxa Locação:</b> {{debitTransaction.rentalFee}}</p>
+            <p><b>TOTAL:</b> {{debitTransaction.amount}}</p>
+          </div>
+        </div>
+        <div class="containerItens">
+          <div class="caixa">
+            <p><b>*Descrição do diversos em crédito*</b></p>
+            <p>{{creditTransaction.sundryDescription}}</p>
+          </div>
+          <div class="caixa">
+            <p><b>*Descrição do diversos em dédito*</b></p>
+            <p>{{debitTransaction.sundryDescription}}</p>
+          </div>
+        </div>
+        <div class="linha">
+          <p style="text-align: center"><b>SALDO:</b> {{creditTransaction.amount}} - {{debitTransaction.amount}}</p>
+        </div>
+        <br />
         <div class="caixa">
-          <p>{{debitTransaction.sundryDescription}}</p>
+          <p style="text-align: center">
+            <b>Banco:</b> {{locator.bank}} / <b>Tipo da conta:</b> {{locator.accountType}}
+          </p>
+          <p style="text-align: center">  
+            <b>Agência:</b> {{locator.agency}} / <b>Conta:</b> {{locator.accountNumber}}
+          </p>
         </div>
-      </div>
-      <div class="linha">
-        <p style="text-align: center"><b>TOTAL CRÉDITO:</b> {{creditTransaction.amount}}</p>
-        <p style="text-align: center"><b>TOTAL DÉBITO:</b> {{debitTransaction.amount}}</p>
-        <p style="text-align: center"><b>SALDO:</b> {{creditTransaction.amount}} - {{debitTransaction.amount}}</p>
-      </div>
-      <br />
-      <div class="caixa">
-        <p style="text-align: center">
-          <b>BANCO:</b> {{locator.bank}} / <b>AGÊNCIA:</b> {{locator.agency}} /
-          <b>CONTA:</b> {{locator.accountNumber}}
-        </p>
-      </div>
-    </section>
-    <section class="section">
-      <header class="cabeca">
-        <img src="https://i.imgur.com/Roi8Q9V.png" />
-        <p>
-          Rua Dr. Carlos Autran N 15 - Centro, LORENA/SP. CEP. 12600-160 TEL
-          .: (12) 3152-1099 / 3152-1541 CRECI- 30558-J
-        </p>
-      </header>
-      <div class="linha">
-        <p style="text-align: center"><b>EXTRATO MENSAL</b></p>
-      </div>
-      <p><b>Locador:</b> {{locator.fullName}}</p>
-      <p><b>Imóvel:</b> {{property.address}}</p>
-      <p><b>Locatário:</b> {{tenant.fullName}}</p>
-      <div class="linhaBaixo"></div>
-      <p><b>Mês de Referência:</b> {{installment.referenceMonth}} / <b>Data de vencimento:</b> {{installment.dueDate}}
-      </p>
-      <p><b>Data Pagamento:</b> {{installment.paymentDate}} / <b>N° PARCELA:</b> {{installment.currentInstallment}} /
-        <b>CÓD:</b>{{property.propertyCode}}</p>
-      <div class="linha">
-        <p style="text-align: center"><b>IDENTIFICAÇÃO DOS ITENS</b></p>
-      </div>
-      <div class="containerItens">
-        <div>
-          <p><b><u>CRÉDITO</b></u></p>
-          <p><b>Aluguel:</b> {{creditTransaction.rent}}</p>
-          <p><b>IPTU:</b> {{creditTransaction.iptu}}</p>
-          <p><b>Água:</b> {{creditTransaction.water}}</p>
-          <p><b>Luz:</b> {{creditTransaction.eletricity}}</p>
-          <p><b>Condomínio:</b> {{creditTransaction.condominium}}</p>
-          <p><b>Imposto de renda:</b> {{creditTransaction.incomeTax}}</p>
-          <p><b>Diversos*:</b> {{creditTransaction.sundry}}</p>
-          <p><b>Multa recisória:</b> {{creditTransaction.breachOfContractFine}}</p>
-        </div>
-        <div>
-          <p><b><u>DÉBITO</b></u></p>
-          <p><b>IPTU:</b> {{debitTransaction.iptu}}</p>
-          <p><b>Água:</b> {{debitTransaction.water}}</p>
-          <p><b>Luz:</b> {{debitTransaction.eletricity}}</p>
-          <p><b>Condomínio:</b> {{debitTransaction.condominium}}</p>
-          <p><b>Imposto de renda:</b> {{debitTransaction.incomeTax}}</p>
-          <p><b>Diversos*:</b> {{debitTransaction.sundry}}</p>
-          <p><b>Taxa ADM:</b> {{debitTransaction.administrationFee}}</p>
-          <p><b>Taxa Locação:</b> {{debitTransaction.rentalFee}}</p>
-        </div>
-      </div>
-      <br />
-      <div class="containerItens">
-        <div class="caixa">
-          <p>{{creditTransaction.sundryDescription}}</p>
-        </div>
-        <div class="caixa">
-          <p>{{debitTransaction.sundryDescription}}</p>
-        </div>
-      </div>
-      <div class="linha">
-        <p style="text-align: center"><b>TOTAL CRÉDITO:</b> {{creditTransaction.amount}}</p>
-        <p style="text-align: center"><b>TOTAL DÉBITO:</b> {{debitTransaction.amount}}</p>
-        <p style="text-align: center"><b>SALDO:</b> {{creditTransaction.amount}} - {{debitTransaction.amount}}</p>
-      </div>
-      <br />
-      <div class="caixa">
-        <p style="text-align: center">
-          <b>BANCO:</b> {{locator.bank}} / <b>AGÊNCIA:</b> {{locator.agency}} /
-          <b>CONTA:</b> {{locator.accountNumber}}
-        </p>
-      </div>
-    </section>
-  </div>
-</body>
-
+      </section>
+    </div>
+  </body>
 </html>`;
 
 interface RentReceiptDefaultProps {
